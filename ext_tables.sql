0f0f9a0f03d39480c4e3dc490022f3691563ce7a
Christian Pschorr <pschorr.christian@gmail.com>
2015-11-29 07:35:34 +0100

    Neues Feld Artikel Abstract auf Seitenebene eingefügt

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,10 +6,3 @@ CREATE TABLE pages (
 	tx_qxarticle_abstract text NOT NULL,
 
 );
-
-#
-# Table structure for table 'pages'
-#
-CREATE TABLE pages (
-	categories int(11) unsigned DEFAULT '0' NOT NULL,
-);
